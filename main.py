3e60b3be44f186d677570c431235da9ca2b8b211
Lukasz Sz <l.szczypin@gmail.com>
2018-01-28 14:13:46 +0100

    GUI introduced, preliminary stage

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import smtplib
 import re
 
-smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-print(smtpObj.ehlo())    
-#print(smtpObj.starttls())
+from tkinter import Tk, Label, Button
+
+class Root():
+    def __init__(self,master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
 
 
-userLogin = input("Please enter Your Login: ")
+    def greet(self):
+        print("Greetings!")
 
-while not re.match(r"[^@]+@[^@]+\.[^@]+", userLogin):
-    print("incorrect email adress ")
+
+
+def sendMail():
+    smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    print(smtpObj.ehlo())    
+    #print(smtpObj.starttls())
+
     userLogin = input("Please enter Your Login: ")
+    while not re.match(r"[^@]+@[^@]+\.[^@]+", userLogin):
+        print("incorrect email adress ")
+        userLogin = input("Please enter Your Login: ")
+
+    userPassword = input("Please enter Your password: ")        
+    smtpObj.login(userLogin,userPassword)
+    smtpObj.sendmail(userLogin, userLogin,'Subject: Hej Koteczku\nKocham Cie. Z powazaniem, Lukaszek')
+                    
 
 
-userPassword = input("Please enter Your password: ") 
 
-    
-smtpObj.login(userLogin,userPassword)
-
-
-smtpObj.sendmail(userLogin, userLogin,'Subject: So long.\nDear Me, so long and thanks for all the fish. \nSincerely, Me')
-                
+if __name__ == "__main__":
+    root = Tk()
+    my_gui = Root(root)
+    root.mainloop()
